cda4bd27967c3289d221cbd6fa11ce5cee5ba5e7
James Leasure <james.leasure@tri-c.edu>
2017-08-31 14:07:26 -0400

    Added grading comments

--- ConsoleApp2/Hello World.cs
+++ ConsoleApp2/Hello World.cs
@@ -10,7 +10,16 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
+            string name = "Jennifer Borowski";
+            string greeting = "Hello";
+
             Console.WriteLine("Hello World from Jennifer Borowski");
+
+            // One way to output the variables' values
+            Console.WriteLine("{0} from {1}", greeting, name);
+
+            // Another way to output their values using string concatenation
+            Console.WriteLine(greeting + " from " + name);
         }
     }
 }
